5c6b662456722d9eb19a5baeb2949c5f98409857
Edson Cabina <edson.cabina@jupiter.co.ao>
2021-04-28 13:46:46 +0100

    Formulario Finalizado

--- resources/views/admin/pages/products/edit.blade.php
+++ resources/views/admin/pages/products/edit.blade.php
@@ -1,12 +1,13 @@
 @extends('admin.layouts.app')
 
-@section('title','Cadastrar Novo Produto')
+@section('title','Editar Produto')
 
 @section('content')
     <h1>Editar Novo Produto {{ $id }}</h1>
 
     <form action="{{ route('produts.update',$id) }}" method="post">
         @csrf
+        @method('PUT')
         <input type="text" name="name" placeholder="Nome:">
         <input type="text" name="description" placeholder="Descrição">
         <button type="submit">Enviar</button>
